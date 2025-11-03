d9194f7f03b06333a118a5f56a18d1392b7c0544
Kilo <95199064+Kiloabqq@users.noreply.github.com>
2025-11-02 21:22:52 -0700

    Update mamController.js

--- controllers/mamController.js
+++ controllers/mamController.js
@@ -2,12 +2,16 @@ const { multiply } = require('../utils/mamLogic');
 
 function multiplyHandler(req, res) {
   const { a, b, mode, metadata } = req.body;
+  console.log('Incoming payload:', { a, b, mode, metadata });
+
   try {
     const result = multiply(a, b, mode, metadata);
     res.json({ result });
   } catch (err) {
-    res.status(400).json({ error: err.message });
+    console.error('Error in multiplyHandler:', err);
+    res.status(500).json({ error: err.message });
   }
 }
+
 
 module.exports = { multiplyHandler };
