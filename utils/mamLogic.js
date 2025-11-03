ac140ca4587644d7f2cd86840a7d85a3dab2d596
Kilo <95199064+Kiloabqq@users.noreply.github.com>
2025-11-02 21:25:22 -0700

    Update mamLogic.js

--- utils/mamLogic.js
+++ utils/mamLogic.js
@@ -3,13 +3,22 @@ function multiply(a, b, mode = 'standard', metadata = {}) {
     throw new Error('Inputs must be numbers');
   }
 
-  if (mode === 'null') return 0;
-  if (mode === 'catalytic') return a + 1;
-  if (mode === 'unit_amplify') return a * b + 1;
-  if (mode === 'doubling_symmetry') return 2 * a;
-  if (mode === 'non_commutative') return a * b + (a !== b ? 1 : 0);
-
-  return a + b;
+  switch (mode) {
+    case 'null':
+      return 0;
+    case 'catalytic':
+      return a + 1;
+    case 'unit_amplify':
+      return a * b + 1;
+    case 'doubling_symmetry':
+      return 2 * a;
+    case 'non_commutative':
+      return a * b + (a !== b ? 1 : 0);
+    case 'standard':
+      return a + b;
+    default:
+      throw new Error(`Unknown mode: ${mode}`);
+  }
 }
 
 module.exports = { multiply };
