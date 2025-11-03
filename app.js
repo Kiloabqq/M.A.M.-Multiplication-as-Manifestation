258bdec80c8217c3d233e9b535a28bfe10feef98
Kilo <95199064+Kiloabqq@users.noreply.github.com>
2025-11-02 21:19:32 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const multiplyRoute = require('./routes/multiply');
-
+app.use('/mam/multiply', multiplyRoute);
 app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/health', (req, res) => res.status(200).send('OK'));
-app.use('/mam/multiply', multiplyRoute);
+
 
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`M.A.M. API running on ${PORT}`));
